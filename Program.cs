d8cc7be3d65afbd17630da262a7ec5bdd74e026d
Yan Lima <71718541+Dev-Yan@users.noreply.github.com>
2021-05-19 14:29:13 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2;
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
 using Google.Apis.Services;
@@ -7,23 +7,48 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
+using System.Data;
+using System.Text;
+using System.Data.SqlClient;
+using System.Collections;
 
 namespace PlanilhaUnico
 {
     class Program
     {
-      
-        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
-        static string ApplicationName = "Google Sheets API .NET Quickstart";
 
+        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        static string ApplicationName = "Planilha-unico";
         static void Main(string[] args)
         {
+
+            void Salvar(IList<object> dados)
+            {
+                using (SqlConnection connection = new SqlConnection("Data Source=UNICO-P90F005;Initial Catalog=master;Integrated Security=True"))
+                {
+                    {
+                        string sql = "INSERT INTO Lojas (MacroCanal) VALUES (@dados0)";
+                        using (SqlCommand cmd = new SqlCommand(sql, connection))
+                        {
+                            connection.Open();
+                            cmd.Parameters.Add("@dados0", SqlDbType.VarChar, 255).Value = dados[0].ToString();
+                            cmd.CommandType = CommandType.Text;
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+
+
+                }
+            }
+
             UserCredential credential;
+
 
             using (var stream =
                 new FileStream("C:/Planilha-Unico1/PlanilhaUnico-1/PlanilhaUnico-1/client_secret_254177701680-1equ9tertind61egl7lv1dkoda0bj466.apps.googleusercontent.com.json", FileMode.Open, FileAccess.Read))
             {
-               
+
                 string credPath = "token.json";
                 credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
                     GoogleClientSecrets.Load(stream).Secrets,
@@ -34,7 +59,6 @@ namespace PlanilhaUnico
                 Console.WriteLine("Credential file saved to: " + credPath);
             }
 
-            
             var service = new SheetsService(new BaseClientService.Initializer()
             {
                 HttpClientInitializer = credential,
@@ -44,19 +68,22 @@ namespace PlanilhaUnico
             String spreadsheetId = "1QR7jwzYaa9EMzfrdSEUvA8geFrNWFvbBPIN7XPt00cQ";
             String range = "Página1!A1:BE28";
             SpreadsheetsResource.ValuesResource.GetRequest request =
-                    service.Spreadsheets.Values.Get(spreadsheetId, range);
+            service.Spreadsheets.Values.Get(spreadsheetId, range);
 
-            
-             https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+
+        https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
             ValueRange response = request.Execute();
             IList<IList<Object>> values = response.Values;
+
+
             if (values != null && values.Count > 0)
             {
-                Console.WriteLine("Name, Major");
+
                 foreach (var row in values)
                 {
-                    
-                    Console.WriteLine("{0}, {1}", row[0], row[4]);
+                    Console.WriteLine("{0}, | {1}, | {2}, | {3}, | {4}", row[0], row[1], row[3], row[4], row[5]);
+                    Salvar(row);
+
                 }
             }
             else
@@ -66,5 +93,8 @@ namespace PlanilhaUnico
             Console.Read();
         }
     }
+
+
+
 }
-// [END sheets_quickstart]
+
